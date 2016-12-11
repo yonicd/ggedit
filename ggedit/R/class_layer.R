b1b49041fc8ed8d99489e6b88077f54ecea1566f
yonicd <yonicd@gmail.com>
2016-12-10 20:03:26 -0500

    fix for tbl and tbl.df class for main data.frame call in ggplot

--- ggedit/R/class_layer.R
+++ ggedit/R/class_layer.R
@@ -4,7 +4,19 @@ class_layer=function(p){
   plot_aes=plot_aes[plot_aes!='NULL']
   plot_aes=data.frame(var=unlist(plot_aes),stringsAsFactors = F)
   plot_aes$aes=aes.nm
-  plot_aes$class=unlist(sapply(plot_aes$var,function(x) class(p$data[,x])))
+  plot_cl=lapply(plot_aes$var,function(x) class(p$data[,x]))
+  
+  chkIdx=which(unlist(lapply(plot_cl,length))!=1)
+  for(i in chkIdx){
+    if('data.frame'%in%plot_cl[[i]]){
+      plot_cl[[i]]='data.frame'
+    }else{
+        break(paste0('classes:c(',paste0(plot_cl[[i]],collapse=','),') for plot variable:', plot_aes$var[i],'. Please choose one and rerun'))
+      }
+  }
+  
+  
+  plot_aes$class=plot_cl
   plot_aes$layer='plot'
   
   layer_aes=lapply(p$layers,function(x) data.frame(var=as.character(x$mapping),aes=names(x$mapping),stringsAsFactors = F))
