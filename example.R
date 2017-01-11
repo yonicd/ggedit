fdf9773a2b5ba7c79bc39e0b498895c34f730360
yonicd <yonicd@gmail.com>
2017-01-11 16:14:08 -0500

    update example file

--- example.R
+++ example.R
@@ -1,32 +1,35 @@
+
 pList=list(
   #1
-  iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
+  point=iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
     geom_point(aes(colour=Species),size=6),
   #2
-  iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
+  pointWrap=iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
     geom_point(aes(colour=Species),size=6)+
     facet_wrap(~Species,scales='free')+labs(title='a'),
   #3
-  iris%>%mutate(Petal2=factor(Petal.Length>2,labels=c("Long Petal",'Short Petal')))%>%
+  boxplotWrap=iris%>%mutate(Petal2=factor(Petal.Length>2,labels=c("Long Petal",'Short Petal')))%>%
     ggplot(aes(x =Species,y=Sepal.Width,fill=Species))+
     geom_boxplot()+facet_wrap(~Petal2,scales='free_x'),
   #4
-  iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
+  pointLine=iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
     geom_point(aes(shape=Species,colour=Petal.Width),size=6)+
     geom_line(linetype=2),
   #5
-  iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
+  pointSmooth=iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
     geom_point(aes(shape=Species,colour=Petal.Width),size=6)+
     geom_smooth(method='loess'),
   #6
-  iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
+  pointLineWrap=iris%>%ggplot(aes(x =Sepal.Length,y=Sepal.Width))+
     geom_point(aes(colour=Species),size=6)+
     geom_line(linetype=2)+
     facet_wrap(~Species,scales='free'),
   #7
-  iris%>%do(.,cbind(.,r=1:nrow(.),id=sample(c('a','b','c','d'),nrow(.),replace = T)))%>%
+  pointLineGrid=iris%>%do(.,cbind(.,r=1:nrow(.),id=sample(c('a','b','c','d'),nrow(.),replace = T)))%>%
     ggplot(aes(x=Sepal.Length,y=Sepal.Width,group=Species))+
     geom_point(aes(colour=id))+geom_line()+
     facet_grid(Species~id)+
     list()
 )
+
+save(pList,file=file.path("ggedit","data","example.rda")) # Expects to run from top of repo
