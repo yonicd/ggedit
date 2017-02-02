ddfbf4f4ea50eb762802dcee727cca9040383e97
yonicd <yonicd@gmail.com>
2017-02-02 09:21:06 -0500

    Update global.R
    remove local paths from global file

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -1,4 +1,3 @@
-.libPaths('/data/jonathans/script/lib')
 library(ggedit)
 
 p.in<-data.frame(x=0,y=0,xend=-1,yend=1,lbl="Waiting for Plot(s)",stringsAsFactors = F)%>%
@@ -14,11 +13,3 @@ if(!all(unlist(lapply(p.in,is.ggplot)))) stop("'object' is not a valid ggplot ob
 if(!exists('minHeight')) minHeight=1000
 uploadEnv=new.env()
 uploadEnv$p.in
-
-#load(file.path(getwd(),'www/functions/appFuns.rda'))
-#list2env(appFuns,envir = .GlobalEnv)
-#appFuns=list.files('www/functions',pattern = '.R')
-# junk=sapply(names(appFuns),function(x) source(file.path('www/functions/',x)))
-# rdas=list.files(pattern = 'rda')
-# for(r in rdas) load(r)
-# rm(r,rdas,junk)
