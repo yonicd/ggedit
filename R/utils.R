3a273debec144dccc5a1869a0ade6200aeb3bd0d
jonathans <yonicd@gmail.com>
2017-05-16 20:53:34 -0400

    update build_map

--- R/utils.R
+++ R/utils.R
@@ -5,12 +5,12 @@
 build_map=function(y){
   item=names(y)
   out=''
-  if(!inherits(y,c('ggproto_method'))) out=paste(item,y,sep="=") 
   if(is.character(y)) out=paste(item,paste0("'",y,"'"),sep="=") 
   if(inherits(y,'formula')) out=paste0("formula=as.formula('",paste0(as.character(y)[-1],collapse="~"),"')")
   if(is.null(y)) out=paste(item,'NULL',sep="=")
   if(inherits(y,c('function','call','ggproto'))) out=paste0(item,'=',paste(utils::capture.output(dput(y)),collapse='\n'))
   if(inherits(y,c('data.frame'))) out=paste0('=',paste(utils::capture.output(dput(y)),collapse='\n'))
   if(inherits(y,c('ggproto_method'))) out=paste0("=get_edges(edges='short')")
+  if(out=='') out=paste(item,y,sep="=")
   return(out)
   }
