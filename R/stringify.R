258498269cd5db38fca2d57c53f3c79b30646c88
jonathans <yonicd@gmail.com>
2017-07-07 09:18:53 -0400

    update stringify to include themes

--- R/stringify.R
+++ R/stringify.R
@@ -8,13 +8,25 @@
 #'  this.gg<-dput.ggedit(pList$pointSmooth) #dput the plot
 #'  writeLines(this.gg) #show the output
 #'  eval(parse(text=this.gg)) #recompile the plot
+#'  
+#'  #add theme change
+#'  p<-pList$pointSmooth+theme(panel.background = element_rect(fill='green'))
+#'  this.gg<-dput.ggedit(p) #dput the plot
+#'  writeLines(this.gg) #show the output
+#'  eval(parse(text=this.gg)) #recompile the plot
+#'  
 #' @rdname stringify.plot
 #' @export 
 
 dput.ggedit<-function(obj,file=""){
-  str.out<-paste0(c(cloneRoot(obj,verbose=TRUE),
-           sapply(obj$layers,cloneLayer,verbose=TRUE,showDefaults = FALSE),
-           cloneFacet(obj$facet,verbose=TRUE)),collapse='+')  
+  
+  objs<-c(cloneRoot(obj,verbose=TRUE),
+          sapply(obj$layers,cloneLayer,verbose=TRUE,showDefaults = FALSE),
+          cloneFacet(obj$facet,verbose=TRUE))
+  
+  if(inherits(obj$theme,'theme')) objs<-c(objs,compare(obj$theme,theme_get()))
+  
+  str.out<-paste0(objs,collapse='+')  
   
   if(nchar(file)>0){
     cat(str.out,file = file) 
