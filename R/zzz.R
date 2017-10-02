ca5b6f13f0fc1a42bad5b7dd430990fc6e626771
Jonathan Sidi <yonicd@gmail.com>
2017-10-02 09:28:38 -0400

    remove redundant shiny hook

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,3 @@
 .onAttach <- function(libname, pkgname) {
   shiny::addResourcePath("sbs", system.file("www", package="shinyBS"))
 }
-
-setHook(packageEvent("shiny", "onLoad"),
-        function(...) shiny::addResourcePath("sbs", system.file("www", package="shinyBS"))
-        )
